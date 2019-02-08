a92c3e3d11676aa6e638b0ffe823761aa8d4fd06
David Cohoe <sudgylacmoe@gmail.com>
2019-02-08 09:45:04 -0800

    Updated copyright year

--- src/main/java/dynamic_parameters/BoolParameter.java
+++ src/main/java/dynamic_parameters/BoolParameter.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Portland State University
+/* Copyright (C) 2019 Portland State University
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of version 3 of the GNU Lesser General Public License
